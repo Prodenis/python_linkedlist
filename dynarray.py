8f029061b45ba27bdd245a9fb5d57ae9a7305ee0
Denis <den.sergeev32@gmail.com>
2018-07-02 20:30:51 +0000

    dynarray insert

---
+++ dynarray.py
@@ -0,0 +1,57 @@
+import ctypes
+
+
+class DynArray:
+
+    def __init__(self):
+        self.count = 0
+        self.capacity = 16
+        self.array = self.make_array(self.capacity)
+
+    def __len__(self):
+        return self.count
+
+    def make_array(self, new_capacity):
+        return (new_capacity * ctypes.py_object)()
+
+    def __getitem__(self, i):
+        if i < 0 or i >= self.count:
+            raise IndexError('Index is out of bounds')
+        return self.array[i]
+
+    def resize(self, new_capacity):
+        new_array = self.make_array(new_capacity)
+        for i in range(self.count):
+            new_array[i] = self.array[i]
+        self.array = new_array
+        self.capacity = new_capacity
+
+    def append(self, itm):
+        if self.count == self.capacity:
+            self.resize(2 * self.capacity)
+        self.array[self.count] = itm
+        self.count += 1
+
+    def insert(self, i, itm):
+        for j in range(self.count):
+            if j == i:
+                old = self.array[j]
+                self.array[j] = itm
+                for j in range(i, self.count):
+                    print(j)
+                    #print(old)
+                    old2 = self.array[j + 1]
+                    #print(old2)
+                    self.array[j+1] = old
+                    old = old2
+                return
+
+da = DynArray()
+for i in range(10):
+    da.append(i)
+    #print(da[i])
+
+da.insert(2, 100)
+
+for i in range(da.__len__()):
+    print(da[i])
